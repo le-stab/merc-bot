022428501cb1421f3a4282af632dd3e24a7dc837
guevarita <guevarita@gmail.com>
2021-02-27 12:24:33 -0300

    Added ID to the results

--- src/ml_bot/ml_user_punisher.py
+++ src/ml_bot/ml_user_punisher.py
@@ -1,18 +1,22 @@
 
-from operator import itemgetter, attrgetter
 import requests
+import json
+
+
 user = 'ORSLOK400'
 api_url = f'https://api.mercadolibre.com/sites/MLA/search?nickname={user}'
 
 r = requests.get(api_url)
-json = r.json()
+json_data = json.loads(r.text)
 
 products = []
 
-for item in json['results']:
-    products.append([item['title'], item['price'], item['sold_quantity'], item['permalink']])
+# print(json.dumps(json_data, indent=2))
 
-sorted_products = sorted(products, key=itemgetter(2), reverse=True)
+for item in json_data['results']:
+    products.append([item['title'], item['price'], item['id'], item['sold_quantity'], item['permalink']])
+
+sorted_products = sorted(products, key=lambda x: x[2], reverse=True)
 
 for item in sorted_products:
     if item[2] != 0:
